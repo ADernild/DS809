1b621d11f9550b7b820f95631b2e674afc35c002
ADernild <alex@dernild.dk>
2021-11-02 11:46:00 +0100

    data sorting script

---
+++ data_sorter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 11:27:54 2021
+
+@author: adernild
+"""
+#%% Libraries
+import os
+import shutil
+import splitfolders
+
+#%% Opdeling af hunde og katte
+# Path til data
+dir = 'Data_science/3_semester/DL/project/data/'
+
+# Looper gennem filer
+for file in os.listdir(dir):
+    dir_name = file[0:3] # subsetter de første tre karaktere "cat" eller "dog"
+    
+    # Opretter path til cat og dogs
+    dir_path = dir + dir_name
+    
+    # Tjekker om path eksisterer (hvis ikke skabes det)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        
+    # Hvis/når det eksisterer tilføjes filen
+    if os.path.exists(dir_path):
+        file_path = dir + file
+        
+        shutil.move(file_path, dir_path)
+
+os.remove('data/ico') # Fjerner mappen ico, der indeholder en .ico fil
+
+#%% Splitter mapper i test, train og val
+splitfolders.ratio(dir, output=dir, seed=42069, ratio=(.8, .1, .1))
+
