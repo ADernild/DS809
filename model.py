dbd46734cd13635d4ab273aea11cdf8af31b847f
C <chrla15@student.sdu.dk>
2021-11-03 11:06:04 +0100

    New

--- model.py
+++ model.py
@@ -24,8 +24,37 @@ from PIL import Image
 import glob
 import pandas as pd
 from keras.models import sequential
-import data_sorter
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+#
+
+filenames = os.listdir("./train")
+
+categories=[]
+
+for f_name in filenames:
+    category = f_name.split('.')[0]
+    if category=='dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+df=pd.DataFrame({'filename': filenames, 'category':categories})
+
+#Train your stuff with patience, lol, otherwise it may burn down your house
+earlystop = EarlyStopping(patience=10)
+learning_rate_red = ReduceLROnPlateau(monitor='val_acc', patience=5, verbose=1, factor=0.5, min_lr=0.00001)
+callbacks = [earlystop, learning_rate_red]
+
+df["category"] = df["category"].replace({0:'cat',1:'dog'})
+training_df, validate_df=train_test_split(df, test_size=0.20, random_state=42)
+training_df = training_df.reset_index(drop=True)
+validate_df=validate_df.reset_index(drop=True)
+
+train_data_gen = ImageDataGenerator(rotation_range=40, rescale=1./255, shear_range=0.1, zoom_range=0.2, horizontal_flip= False, vertical_flip=True, width_shift_range=0.1, height_shift_range=0.1)
+train_gen = train_data_gen.flow_from_dataframe(training_df, "./train/dog", "./train/cat", x_col='filename', y_col='category', target_size=imagex%imagey, class_mode='categorical', batch_size=batch)
+
+
 
 imagex = 200
 imagey = 200
@@ -55,26 +84,3 @@ model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['ac
 model.summary()
 
 
-filenames=os.listdir("./train")
-categories=[]
-for f_name in filenames:
-    category = f_name.split('.')[0]
-    if category=='dog':
-        categories.append(1)
-    else
-        categories.append(0)
-
-df=pd.DataFrame({'filename:'filenames, 'category':categories})
-
-#Train your stuff with patience, lol, otherwise it may burn down your house
-earlystop = EarlyStopping(patience=10)
-learning_rate_red = ReduceLROnPlateau(monitor='val_acc', patience=5, verbose=1, factor=0.5, min_lr=0.00001)
-callbacks = [earlystop, learning_rate_red]
-
-df["category"] = df["category"].replace({0:'cat',1:'dog'})
-training_df, validate_df=train_test_split(df, test_size=0.20, random_state=42)
-training_df = training_df.reset_index(drop=True)
-validate_df=validate_df.reset_index(drop=True)
-
-train_data_gen = ImageDataGenerator(rotation_range=40, rescale=1./255, shear_range=0.1, zoom_range=0.2, horizontal_flip= False, vertical_flip=True, width_shift_range=0.1, height_shift_range=0.1)
-train_gen = train_data_gen.flow_from_dataframe(training_df, "./train/dog", "./train/cat", x_col='filename', y_col='category', target_size=Image_size, class_mode='categorical', batch_size=batch)
