b6985c981fe279c2e3b89162e09f84143d5cf542
ADernild <alex@dernild.dk>
2021-11-03 12:07:45 +0100

    flow_from_directory

--- model.py
+++ model.py
@@ -28,6 +28,15 @@ import pandas as pd
 from keras.models import sequential
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
+#%% Libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#%%
+
+
 #Issue; Unsure if every image in /train/cat and /train/dog is "imported", or if it's just pointing on /train and the two folders(?)
 #brain not working atm to figure this out or fix it
 
@@ -56,6 +65,7 @@ training_df, validate_df=train_test_split(df, test_size=0.20, random_state=42)
 training_df = training_df.reset_index(drop=True) #Unsure if needed - reset on index + drop
 validate_df=validate_df.reset_index(drop=True)
 
+
 imagex = 200
 imagey = 200
 image_size = [imagex + imagey]
@@ -65,29 +75,61 @@ train_data_gen = ImageDataGenerator(rotation_range=40, rescale=1./255, shear_ran
 train_gen = train_data_gen.flow_from_dataframe(training_df, "./train", x_col='filename', y_col='category', target_size=image_size, class_mode='categorical', batch_size=batch)
 
 
-model = keras.Sequential()
+#%% flow_from_directory
+imagex = 200
+imagey = 200
+image_size = [imagex + imagey]
+batch = 30
+
+train_datagen = ImageDataGenerator(rotation_range=40,
+                                   rescale=1./255,
+                                   shear_range=0.1,
+                                   zoom_range=0.2,
+                                   horizontal_flip= False,
+                                   vertical_flip=True,
+                                   width_shift_range=0.1,
+                                   height_shift_range=0.1)
+
+train_gen = train_datagen.flow_from_directory(
+    'train',
+    target_size=image_size,
+    batch_size=batch,
+    class_mode='binary')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+val_generator = test_datagen.flow_from_directory(
+        'val',
+        target_size=image_size,
+        batch_size=batch,
+        class_mode='binary')
+
+#%% Giver mig en Typeerror
+model = keras.Sequential([
         #Layers, Pooling, Dropout and Density
-layers.Conv2D(32, 3, padding='same', activation='relu', input_shape=(image_size, 3)),
-layers.Conv2D(32, 3, padding='same', activation='relu'),
-layers.MaxPooling2D(2,2),
-layers.Dropout(0.2),
+    layers.Conv2D(32, 3, padding='same', activation='relu', input_shape=(image_size, 3)),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Dropout(0.2),
 
-layers.Conv2D(64, 3, padding='same', activation='relu'),
-layers.Conv2D(64, 3, padding='same', activation='relu'),
-layers.MaxPooling2D(2, 2),
-layers.Dropout(0.2),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Dropout(0.2),
 
-layers.Conv2D(128, 3, padding='same', activation='relu'),
-layers.Conv2D(128, 3, padding='same', activation='relu'),
-layers.MaxPooling2D(2, 2),
-layers.Dropout(0.2),
+    layers.Conv2D(128, 3, padding='same', activation='relu'),
+    layers.Conv2D(128, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Dropout(0.2),
 
-layers.Dense(1, activation='sigmoid')
-model.fit(image_size, batch_size=30, epochs=100)
+    layers.Dense(1, activation='sigmoid')
+])
 
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 model.summary()
 
-
+#%%
+model.fit(image_size, batch_size=30, epochs=100)
 # Needed: Plots/ visuals to show the training i.e. to be implemented
 
