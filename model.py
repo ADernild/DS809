1c093a028c0da42bc3b4e52fc80581e95c362655
C <chrla15@student.sdu.dk>
2021-11-03 11:11:51 +0100

    New

--- model.py
+++ model.py
@@ -26,11 +26,13 @@ import pandas as pd
 from keras.models import sequential
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
-#
+#Issue; Unsure if every image in /train/cat and /train/dog is "imported", or if it's just pointing on /train and the two folders(?)
+#brain not working atm to figure this out or fix it
 
-filenames = os.listdir("./train")
 
-categories=[]
+filenames = os.listdir("./train") #<---
+
+categories=[] #Empty
 
 for f_name in filenames:
     category = f_name.split('.')[0]
@@ -39,26 +41,33 @@ for f_name in filenames:
     else:
         categories.append(0)
 
-df=pd.DataFrame({'filename': filenames, 'category':categories})
+df=pd.DataFrame({'filename': filenames, 'category':categories}) #Dataframe (df)
 
 #Train your stuff with patience, lol, otherwise it may burn down your house
 earlystop = EarlyStopping(patience=10)
 learning_rate_red = ReduceLROnPlateau(monitor='val_acc', patience=5, verbose=1, factor=0.5, min_lr=0.00001)
 callbacks = [earlystop, learning_rate_red]
 
+#Df from earlier
 df["category"] = df["category"].replace({0:'cat',1:'dog'})
 training_df, validate_df=train_test_split(df, test_size=0.20, random_state=42)
-training_df = training_df.reset_index(drop=True)
+training_df = training_df.reset_index(drop=True) #Unsure if needed - reset on index + drop
 validate_df=validate_df.reset_index(drop=True)
-
-train_data_gen = ImageDataGenerator(rotation_range=40, rescale=1./255, shear_range=0.1, zoom_range=0.2, horizontal_flip= False, vertical_flip=True, width_shift_range=0.1, height_shift_range=0.1)
-train_gen = train_data_gen.flow_from_dataframe(training_df, "./train/dog", "./train/cat", x_col='filename', y_col='category', target_size=imagex%imagey, class_mode='categorical', batch_size=batch)
-
-
 
 imagex = 200
 imagey = 200
+image_size = [imagex + imagey]
 batch = 30
+
+train_data_gen = ImageDataGenerator(rotation_range=40, rescale=1./255, shear_range=0.1, zoom_range=0.2, horizontal_flip= False, vertical_flip=True, width_shift_range=0.1, height_shift_range=0.1)
+train_gen = train_data_gen.flow_from_dataframe(training_df, "./train/dog", "./train/cat", x_col='filename', y_col='category', target_size=image_size, class_mode='categorical', batch_size=batch)
+
+
+
+#imagex = 200
+#imagey = 200
+#image_size = [imagex + imagey] # Moved up
+#batch = 30
 
 model = keras.Sequential()
         #Layers, Pooling, Dropout and Density
@@ -83,4 +92,6 @@ model.fit(imagex, imagey, batch_size=30, epochs=100)
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 model.summary()
 
+
+# Needed: Plots/ visuals to show the training i.e. to be implemented
 
